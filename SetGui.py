6cbfb855e104e5cbc6e3666765265937715570cf
marv nwadike <marvnwadike5@gmail.com>
2021-11-04 18:52:20 -0700

    ability to copy password:

--- SetGui.py
+++ SetGui.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PassWordGenrate import PasswordGenerator
 import tkinter as tk
+import pyperclip as pc
 class SetGui:
     Window:Tk
     def __init__(self,GuiWindow= Tk()):
@@ -36,11 +37,15 @@ class SetGui:
             self.bEnableSpecialCharacter =False
 
     def Generatepassword(self):
+        if not  self.TextInput.get(): 
+            return 
         PasswordGenrate =PasswordGenerator(self.bEnableLowerCase,self.bEnableCapitalCase,self.bEnableSpecialCharacter,int( self.TextInput.get()))
         self.Password = PasswordGenrate.GeneratePassword()
         self.tkPassword =self.Password
         self.thepassword.config(text = self.Password) 
 
+    def Copy(self):
+        pc.copy(self.Password)
     def Mainloop(self):
         input  = tk.Label(self.Window,text ="Size of password").grid(row =0)
 
@@ -55,13 +60,16 @@ class SetGui:
         LowerCaseLetter =Checkbutton(self.Window,text="Enable Lower case letters",variable=self.EnableLowerCase,command=self.lowercase)
         CaptialCaseLetter1 =Checkbutton(self.Window,text="Enable captial case letters",variable=self.EnableCapitalCase,command=self.CaptialCaseLetter)
         SpecialCharacter =Checkbutton(self.Window,text="Enable special characters",variable=self.EnableSpecialCharacter,command = self.SpecialCharacter)
-
+        copyButton =Button(self.Window,text="Copy",command = self.Copy)
         LowerCaseLetter.place(x=0,y=30)
         CaptialCaseLetter1.place(x=0,y=60)
         SpecialCharacter.place(x=0,y=90)
-        btn.place(x=80,y=120)
-        temp.place(x=40,y=150)
-        self.thepassword.place(x=100,y=150)
+
+        copyButton.place(x=40,y=120)
+        btn.place(x=80,y=150)
+        temp.place(x=40,y=180)
+
+        self.thepassword.place(x=100,y=180)
 
         self.Window.title('password Generator')
         self.Window.geometry("300x200+10+20")
